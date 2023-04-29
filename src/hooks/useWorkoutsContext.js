e72aaa31a6d08df3470b4395d5eadd2b14ec81d3
riddhimanpaul0 <riddhimanpaul0@gmail.com>
2023-04-28 20:38:09 -0400

    frontend

---
+++ src/hooks/useWorkoutsContext.js
@@ -0,0 +1,26 @@
+// import { WorkoutsContext } from "../context/WorkoutsContext";
+// import { useContext } from "react";
+
+// export const useWorkoutsContext = () => {
+//     const context = useContext(WorkoutsContext)
+
+//     if(!context){
+//         throw Error('useWorkoutsContext must be used inside an WorkoutsContextProvider')
+//     }
+
+//     return context
+
+// }
+
+import { WorkoutsContext } from "../context/WorkoutsContext"
+import { useContext } from "react"
+
+export const useWorkoutsContext = () => {
+  const context = useContext(WorkoutsContext)
+
+  if(!context) {
+    throw Error('useWorkoutsContext must be used inside an WorkoutsContextProvider')
+  }
+
+  return context
+}
