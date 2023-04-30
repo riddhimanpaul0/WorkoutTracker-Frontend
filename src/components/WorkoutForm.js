37ca64812d55de6856e8b61aa2020a68c2b242cb
riddhimanpaul0 <riddhimanpaul0@gmail.com>
2023-04-30 12:28:44 -0400

    changing fetch requests

--- src/components/WorkoutForm.js
+++ src/components/WorkoutForm.js
@@ -12,7 +12,7 @@ const WorkoutForm = () => {
     const handleSubmit = async(e) => {
         e.preventDefault()
         const workout = {title, load, reps}
-        const response = await fetch('http://localhost:4000/api/workouts', {
+        const response = await fetch('https://workouttrackerbackendapi.onrender.com/api/workouts', {
             method: 'POST',
             body: JSON.stringify(workout),
             headers: {
